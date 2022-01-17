7646988b304573e73f4f328507d0982af987f5c3
HuzaifaUmair <96041781+HuzaifaUmair@users.noreply.github.com>
2022-01-17 09:27:13 -0800

    Add files via upload

---
+++ Logout.java
@@ -0,0 +1,19 @@
+package com.example.smartparking;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+
+public class Logout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_logout);
+    }
+
+
+}
